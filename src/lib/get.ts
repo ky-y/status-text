64a47ac59d3d5e8ee0a3fea3dba4f3a9d829bc4d
kyoya0819 <kyoya0819@gmail.com>
2024-05-23 05:44:06 +0900

    fix

--- src/lib/get.ts
+++ src/lib/get.ts
@@ -1,9 +1,9 @@
 import * as statusCodes from "./list";
 
 export const statusText = (statusCode: number): string => {
-    const key = "HTTP" + statusCode;
+    const key = "HTTP" + String(statusCode);
 
-    if (Object.keys(statusCode).includes(key))
+    if (Object.keys(statusCodes).includes(key))
         return statusCodes[key as keyof typeof statusCodes];
 
     return "Unknown Status";
